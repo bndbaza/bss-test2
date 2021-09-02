1d84b1a7c50a229188b3eae88ec1da575619dacd
viktor <bnd-baza@mail.ru>
2021-09-02 14:43:16 +0800

    update

---
+++ blog/routers/blog.py
@@ -0,0 +1,48 @@
+from blog import database
+from typing import List
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from .. import schemas, database, models
+
+router = APIRouter(
+    tags=['blogs']
+)
+
+get_db = database.get_db
+
+@router.get('/blog', response_model=List[schemas.ShowBlog])
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@router.post('/blog')
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title,body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.ShowBlog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"not found id {id}")
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
+@router.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"no id {id}")
+    return blog
