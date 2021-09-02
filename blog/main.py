5ef955c52abbe5209ee3493ac1ee1ed70dcfeba3
viktor <bnd-baza@mail.ru>
2021-09-02 14:42:56 +0800

    update

--- blog/main.py
+++ blog/main.py
@@ -1,52 +1,10 @@
-from fastapi import FastAPI, Depends, responses, status, Response, HTTPException
+from fastapi import FastAPI
 from . import schemas, models
-from .database import SessionLocal, engine
-from sqlalchemy.orm import Session
-from typing import List
+from .database import engine
+from .routers import blog
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.post('/blog')
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title,body=request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    return 'done'
-
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.ShowBlog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"not found id {id}")
-    blog.update(request.dict())#({'title': request.title, 'body': request.body})
-    db.commit()
-    return 'updated'
-
-@app.get('/blog', status_code=status.HTTP_201_CREATED, response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
-@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
-def show(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"no id {id}")
-    return blog
+app.include_router(blog.router)
